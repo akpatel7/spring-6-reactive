669ef66921815e5bde4eafeb9773a7be398d4fda
Amit Patel <amitpatel1701@yahoo.com>
2024-03-23 16:57:13 -0400

    276. Add update beer

--- src/main/java/guru/springframework/spring6reactive/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/spring6reactive/services/BeerServiceImpl.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6reactive.services;
 
+import guru.springframework.spring6reactive.domain.Beer;
 import guru.springframework.spring6reactive.mappers.BeerMapper;
 import guru.springframework.spring6reactive.model.BeerDTO;
 import guru.springframework.spring6reactive.repositories.BeerRepository;
@@ -30,6 +31,20 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public Mono<BeerDTO> saveNewBeer(BeerDTO beerDTO) {
         return beerRepository.save(beerMapper.beerDTOToBeer(beerDTO))
+                .map(beerMapper::beerToBeerDTO);
+    }
+
+    @Override
+    public Mono<BeerDTO> updateBeer(Integer beerId, BeerDTO beerDTO) {
+        return beerRepository.findById(beerId)
+                .map(foundBeer -> {
+                    // update properties
+                    foundBeer.setBeerName(beerDTO.getBeerName());
+                    foundBeer.setBeerStyle(beerDTO.getBeerStyle());
+                    foundBeer.setPrice(beerDTO.getPrice());
+                    foundBeer.setUpc(beerDTO.getUpc());
+                            return foundBeer;
+                }).flatMap(beerRepository::save)
                 .map(beerMapper::beerToBeerDTO);
     }
 }
